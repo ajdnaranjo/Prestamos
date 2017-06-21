361bab19beb13303dcfae286a88c09c001920d12
junaranjo <junaranjo@CLDJUNARANJO03>
2017-06-20 22:02:54 -0500

    Adding movimientos

--- Prestamos/Consultas/frmMovimiento.Designer.cs
+++ Prestamos/Consultas/frmMovimiento.Designer.cs
@@ -34,26 +34,39 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
+            this.txtTotalPrestamos = new System.Windows.Forms.TextBox();
+            this.txtTotalPagos = new System.Windows.Forms.TextBox();
+            this.txtTotalAbonos = new System.Windows.Forms.TextBox();
+            this.dtpFechaini = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaFinal = new System.Windows.Forms.DateTimePicker();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.btnBuscar);
+            this.groupBox1.Controls.Add(this.dtpFechaFinal);
+            this.groupBox1.Controls.Add(this.dtpFechaini);
+            this.groupBox1.Controls.Add(this.txtTotalAbonos);
+            this.groupBox1.Controls.Add(this.txtTotalPagos);
+            this.groupBox1.Controls.Add(this.txtTotalPrestamos);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(13, 47);
+            this.groupBox1.Location = new System.Drawing.Point(13, 37);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(335, 156);
+            this.groupBox1.Size = new System.Drawing.Size(313, 163);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Location = new System.Drawing.Point(6, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(69, 13);
             this.label1.TabIndex = 0;
@@ -62,16 +75,17 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 115);
+            this.label2.Location = new System.Drawing.Point(6, 131);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(72, 13);
             this.label2.TabIndex = 1;
             this.label2.Text = "Total abonos:";
+
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 91);
+            this.label3.Location = new System.Drawing.Point(6, 102);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(66, 13);
             this.label3.TabIndex = 2;
@@ -80,7 +94,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 63);
+            this.label4.Location = new System.Drawing.Point(6, 73);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(85, 13);
             this.label4.TabIndex = 3;
@@ -89,20 +103,86 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 38);
+            this.label5.Location = new System.Drawing.Point(6, 48);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(62, 13);
             this.label5.TabIndex = 4;
             this.label5.Text = "Fecha final:";
             // 
+            // txtTotalPrestamos
+            // 
+            this.txtTotalPrestamos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalPrestamos.Location = new System.Drawing.Point(93, 68);
+            this.txtTotalPrestamos.Name = "txtTotalPrestamos";
+            this.txtTotalPrestamos.ReadOnly = true;
+            this.txtTotalPrestamos.Size = new System.Drawing.Size(100, 23);
+            this.txtTotalPrestamos.TabIndex = 5;
+            // 
+            // txtTotalPagos
+            // 
+            this.txtTotalPagos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalPagos.Location = new System.Drawing.Point(93, 97);
+            this.txtTotalPagos.Name = "txtTotalPagos";
+            this.txtTotalPagos.ReadOnly = true;
+            this.txtTotalPagos.Size = new System.Drawing.Size(100, 23);
+            this.txtTotalPagos.TabIndex = 6;
+            // 
+            // txtTotalAbonos
+            // 
+            this.txtTotalAbonos.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalAbonos.Location = new System.Drawing.Point(93, 126);
+            this.txtTotalAbonos.Name = "txtTotalAbonos";
+            this.txtTotalAbonos.ReadOnly = true;
+            this.txtTotalAbonos.Size = new System.Drawing.Size(100, 23);
+            this.txtTotalAbonos.TabIndex = 7;
+            
+            // 
+            // dtpFechaini
+            // 
+            this.dtpFechaini.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaini.Location = new System.Drawing.Point(93, 16);
+            this.dtpFechaini.Name = "dtpFechaini";
+            this.dtpFechaini.Size = new System.Drawing.Size(100, 20);
+            this.dtpFechaini.TabIndex = 8;
+            // 
+            // dtpFechaFinal
+            // 
+            this.dtpFechaFinal.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpFechaFinal.Location = new System.Drawing.Point(93, 42);
+            this.dtpFechaFinal.Name = "dtpFechaFinal";
+            this.dtpFechaFinal.Size = new System.Drawing.Size(100, 20);
+            this.dtpFechaFinal.TabIndex = 9;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(220, 16);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(313, 23);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Consultar movimiento";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // frmMovimiento
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(399, 373);
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(337, 206);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.groupBox1);
             this.Name = "frmMovimiento";
-            this.Text = "frmMovimiento";
+            this.Text = "Consultar movimiento";
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
@@ -117,5 +197,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpFechaFinal;
+        private System.Windows.Forms.DateTimePicker dtpFechaini;
+        private System.Windows.Forms.TextBox txtTotalAbonos;
+        private System.Windows.Forms.TextBox txtTotalPagos;
+        private System.Windows.Forms.TextBox txtTotalPrestamos;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label6;
     }
 }
--- Prestamos/Consultas/frmMovimiento.cs
+++ Prestamos/Consultas/frmMovimiento.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Prestamos.Repositorios;
 
 namespace Prestamos.Consultas
 {
@@ -16,5 +17,20 @@ namespace Prestamos.Consultas
         {
             InitializeComponent();
         }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            var repoPrestamos = new RepositorioCrearPrestamo();
+            var repoPagos = new RepositorioPagos();
+
+            txtTotalPrestamos.Text = 
+                repoPrestamos.PrestamoxFecha(DateTime.Parse(dtpFechaini.Text), DateTime.Parse(dtpFechaFinal.Text)).ToString("N");
+            txtTotalPagos.Text =
+                repoPagos.TotalPagosxDia(DateTime.Parse(dtpFechaini.Text), DateTime.Parse(dtpFechaFinal.Text)).ToString("N");
+            txtTotalAbonos.Text = 
+                repoPagos.NoAbonosXDia(DateTime.Parse(dtpFechaini.Text), DateTime.Parse(dtpFechaFinal.Text)).ToString();
+        }
+
+      
     }
 }
